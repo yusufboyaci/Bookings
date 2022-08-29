b2c22590d3cc3ddd98cc52a54f6bcdbaef5894c1
yusufboyaci <boyacyusuf@gmail.com>
2022-08-29 05:38:11 +0300

    29082022 05:37

---
+++ API/Controllers/ApartmantController.cs
@@ -0,0 +1,52 @@
+﻿using DataAccess.Repositories;
+using DataAccess;
+using Entities.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApartmantController : Controller
+    {
+        RentedApartmantRepository db;
+        public ApartmantController(AppDbContext context)
+        {
+            db = new RentedApartmantRepository(context);
+        }
+        [HttpGet("List")]
+        public IActionResult List()
+        {
+            List<RentedApartmant> list = db.GetActive();
+            return Json(list);
+        }
+        [HttpGet("Get")]
+        public IActionResult Get(Guid id)
+        {
+            return Json(db.GetById(id));
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(RentedApartmant apartmant)
+        {
+            db.Add(apartmant);
+            db.Activate(apartmant.Id);
+            return Json("Ok");
+        }
+        [HttpPut("Update")]
+        public IActionResult Update(RentedApartmant apartmant)
+        {
+            db.Update(apartmant);
+            db.Activate(apartmant.Id);
+            return Json("Ok");
+        }
+        [HttpDelete("Delete")]
+        public IActionResult Delete(RentedApartmant apartmant)
+        {
+            db.Remove(apartmant);
+            return Json("Ok");
+        }
+    }
+}
---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using Entities.Entities;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        UserRepository db;
+        public UserController(AppDbContext context)
+        {
+            db = new UserRepository(context);
+        }
+        [HttpGet("List")]
+        public IActionResult List()
+        {
+            List<User> list = db.GetActive();
+            return Json(list);
+        }
+        [HttpGet("Get")]
+        public IActionResult Get(Guid id)
+        {
+            return Json(db.GetById(id));
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(User user)
+        {
+            db.Add(user);
+            db.Activate(user.Id);
+            return Json("Ok");
+        }
+        [HttpPut("Update")]
+        public IActionResult Update(User user)
+        {
+            db.Update(user);
+            db.Activate(user.Id);
+            return Json("Ok");
+        }
+        [HttpDelete("Delete")]
+        public IActionResult Delete(User user)
+        {
+            db.Remove(user);
+            return Json("Ok");
+        }
+    }
+}
